f63fbfced162e6abffc4ad9c3624e4901f11ebe0
Shane Baek <hobas9626@gmail.com>
2015-01-12 20:54:46 -0500

    cleaned up

--- js/slider-gallery.js
+++ js/slider-gallery.js
@@ -1,11 +1,7 @@
 $(function() {
 	
-	var slidewidth = $('.slide').width(),
-<<<<<<< HEAD
-	    slideCount = $(',slide').size(),
-=======
+	var slideWidth = $('.slide').width(),
 	    slideCount = $('.slide').size(),
->>>>>>> slider-gallery
 	    pos = 0;
 
 	// set the width of the slide tray
